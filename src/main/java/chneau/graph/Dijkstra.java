c6a217b52413fa1660bbbb75a58217583f055761
Enya Goni (csdm) <EG10365@rgu.ac.uk>
2020-02-17 14:56:39 +0000

    calculate cost

--- src/main/java/chneau/graph/Dijkstra.java
+++ src/main/java/chneau/graph/Dijkstra.java
@@ -2,6 +2,7 @@ package chneau.graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -96,5 +97,68 @@ public class Dijkstra {
             return;
         }
         toVisit.add(i + 1, v);
+    }
+
+    private static int calculateCost(Date clock, List<String> transportModes, int from, int to, HashMap<Integer, Info> vertices){
+        var time = clock.getTime();
+        var X = 0; //update
+        double trafficFactor;
+        if(time < X){
+            //we also have to take into account location
+            trafficFactor = 1.5;
+        }else{
+            trafficFactor = 1.1;
+        }
+        var redLightsTime = 0; //update
+        if (transportModes.contains("car") && transportModes.contains("bus")){
+            var cost1 = calcCarTime(from, to, vertices, trafficFactor, redLightsTime);
+            var cost2 = calcBusTime(time, from, to, vertices, trafficFactor, redLightsTime);
+            return getMin(cost1, cost2);
+        }else if (transportModes.contains("car")){
+            var cost = calcCarTime(from, to, vertices, trafficFactor, redLightsTime);
+            return cost;
+        }else if (transportModes.contains("bus")){
+            var cost = calcBusTime(time, from, to, vertices, trafficFactor, redLightsTime);
+            return cost;
+        }else{ //more transport modes?
+            var cost1 = calcCarTime(from, to, vertices, trafficFactor, redLightsTime);
+            var cost2 = calcBusTime(time, from, to, vertices, trafficFactor, redLightsTime);
+            var cost3 = calcBicycleTime(from, to, vertices, trafficFactor, redLightsTime);
+            return getMin(cost1, cost2, cost3);
+        }
+    }
+
+    private static int calcCarTime(int from, int to, HashMap<Integer, Info> vertices, double trafficFactor,
+            int redLightsTime) {
+        var drivingTime = calcDistance(from, to, vertices)/35;
+        return (int) (trafficFactor * (redLightsTime + drivingTime));
+    }
+
+   private static int calcBusTime(long time, int from, int to, HashMap<Integer, Info> vertices, double trafficFactor,
+            int redLightsTime) {
+       var busStop = 0; //location
+       var walkToBusTime = calcDistance(from, busStop, vertices)/3;
+       var busArrivingTime = 0;
+       var waitForBusTime = (int) (busArrivingTime - (time + walkToBusTime));
+       var busTime = calcDistance(busStop, to, vertices)/25;
+       return (int) (trafficFactor * (redLightsTime + busTime) + walkToBusTime + waitForBusTime);
+    }
+
+    private static int calcBicycleTime(int from, int to, HashMap<Integer, Info> vertices, double trafficFactor,
+            int redLightsTime) {
+        var ridingTime = calcDistance(from, to, vertices)/10;
+        return (int) (trafficFactor * (redLightsTime + ridingTime));
+    }
+
+    private static int calcDistance(int from, int to, HashMap<Integer, Info> vertices) {
+        return 0;
+    }
+
+     private static int getMin(int x, int y) {
+        return Math.min(x, y);
+    }
+
+    private static int getMin(int x, int y, int z) {
+        return Math.min(Math.min(x, y), z);
     }
 }
