bf6454ee26da872d39468f3004bba19cec43026b
chneau <charles63500@gmail.com>
2020-02-20 11:40:58 +0000

    GTFS extract = ok

--- src/main/java/chneau/graph/GTFS.java
+++ src/main/java/chneau/graph/GTFS.java
@@ -4,13 +4,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.zip.ZipInputStream;
 import org.apache.commons.csv.CSVFormat;
 
 public class GTFS {
+    public HashMap<String, Map<String, StopTime>> stopTimes = new HashMap<>(); // trip_id + stop_id
+    public HashMap<String, Calendar> calendars = new HashMap<>(); // trip_id
+    public HashMap<String, Stop> stops = new HashMap<>(); // stop_id
+
     public static class Calendar {
         public boolean[] days = new boolean[7];
         public LocalDateTime start;
@@ -40,9 +48,11 @@ public class GTFS {
         var zis = new ZipInputStream(fis);
         var isr = new InputStreamReader(zis);
 
-        var calendars = new HashMap<String, Calendar>(); // service_id
         var stops = new HashMap<String, Stop>(); // stop_id
-        var stopTimes = new HashMap<String, StopTime>(); // trip_id+stop_id
+        var stopTimes = new HashMap<String, Map<String, StopTime>>(); // trip_id stop_id
+
+        var trips = new HashMap<String, Set<String>>(); // service_id
+        var calendars = new HashMap<String, Calendar>(); // service_id
 
         var yyyymmdd = DateTimeFormatter.ofPattern("yyyyMMdd");
 
@@ -60,8 +70,12 @@ public class GTFS {
                             c.days[4] = bool(record.get("friday"));
                             c.days[5] = bool(record.get("saturday"));
                             c.days[6] = bool(record.get("sunday"));
-                            c.start = LocalDateTime.parse(record.get("start_date"), yyyymmdd);
-                            c.end = LocalDateTime.parse(record.get("end_date"), yyyymmdd);
+                            c.start =
+                                    LocalDate.parse(record.get("start_date"), yyyymmdd)
+                                            .atStartOfDay();
+                            c.end =
+                                    LocalDate.parse(record.get("end_date"), yyyymmdd)
+                                            .atStartOfDay();
                             calendars.put(record.get("service_id"), c);
                         }
                     }
@@ -71,8 +85,8 @@ public class GTFS {
                         var csvParser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(isr);
                         for (var record : csvParser) {
                             var s = new Stop();
-                            s.lat = Double.parseDouble("stop_lat");
-                            s.lng = Double.parseDouble("stop_lon");
+                            s.lat = Double.parseDouble(record.get("stop_lat"));
+                            s.lng = Double.parseDouble(record.get("stop_lon"));
                             stops.put(record.get("stop_id"), s);
                         }
                     }
@@ -84,7 +98,10 @@ public class GTFS {
                             var s = new StopTime();
                             s.arrival = record.get("arrival_time");
                             s.departure = record.get("departure_time");
-                            stopTimes.put(record.get("trip_id") + record.get("stop_id"), s);
+                            var tID = record.get("trip_id");
+                            var v = stopTimes.getOrDefault(tID, new HashMap<>());
+                            v.put(record.get("stop_id"), s);
+                            stopTimes.put(tID, v);
                         }
                     }
                     break;
@@ -92,14 +109,37 @@ public class GTFS {
                     {
                         var csvParser = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(isr);
                         for (var record : csvParser) {
-                            System.out.println(record.get("service_id"));
-                            System.out.println(record.get("trip_id"));
+                            var sID = record.get("service_id");
+                            var val = trips.getOrDefault(sID, new HashSet<>());
+                            val.add(record.get("trip_id"));
+                            trips.put(sID, val);
                         }
                     }
                     break;
             }
         }
+
+        var calendarsByTrip = new HashMap<String, Calendar>(); // trip_id
+        for (var e : trips.entrySet()) {
+            var serviceID = e.getKey();
+            var tripIDs = e.getValue();
+
+            for (var tripID : tripIDs) {
+                calendarsByTrip.put(tripID, calendars.get(serviceID));
+            }
+        }
+        System.out.println(calendarsByTrip.size());
         zis.close();
-        return null;
+        var gtfs = new GTFS();
+        gtfs.calendars = calendarsByTrip;
+        gtfs.stopTimes = stopTimes;
+        gtfs.stops = stops;
+        return gtfs;
+    }
+
+    public Graph toGraph() {
+        var g = new Graph();
+
+        return g;
     }
 }
