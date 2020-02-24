a56ed96ebdc49842f8687cc82420b71b12be2509
chneau <charles63500@gmail.com>
2020-02-24 10:52:21 +0000

    ok

--- src/main/java/chneau/graph/GTFSEdge.java
+++ src/main/java/chneau/graph/GTFSEdge.java
@@ -3,9 +3,14 @@ package chneau.graph;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import chneau.timetable.TimeTable;
+
 /** GTFSEdge */
 public class GTFSEdge implements Cost {
+    public TimeTable tt;
     public Duration getCost(LocalDateTime c) {
+        // var t = new TimeTable(1, OpenHours.parse("mo 06:00-16:00"));
+
         return null;
     }
 }
